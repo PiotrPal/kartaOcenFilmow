3d1543867444ad7f918b3ec5f448916bb9efca7d
PiotrPal <133272105+PiotrPal@users.noreply.github.com>
2024-04-18 00:14:29 +0200

    Exceptions

--- KartaPodstawowa.cs
+++ KartaPodstawowa.cs
@@ -5,34 +5,56 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace kartaOcenFilmow {
-    public abstract class KartaPodstawowa : object, IKartaPodstawowa{
+    public abstract class KartaPodstawowa : object, IKartaPodstawowa {
         public abstract void AddOceny(float ocena);
         public abstract KartaStatystyki ObliczStatystyki();
+        
         protected string _nazwa;
 
         //wlasciwosc
+        //public string Nazwa {
+        //    get {
+        //        return _nazwa.ToUpper();
+        //    }
+        //    set {
+        //        if (!string.IsNullOrWhiteSpace(value)) {
+        //            if (_nazwa != value) {
+        //                ZmianaNazwyEventArgs args = new ZmianaNazwyEventArgs();
+        //                args.IstniejacaNazwa = _nazwa;
+        //                args.NowaNazwa = value;
+
+        //                ZmianaNazwy(this, args);
+        //            }
+        //            _nazwa = value;
+        //        }
+        //    }
+        //}
+
         public string Nazwa {
             get {
                 return _nazwa.ToUpper();
             }
             set {
-                if (!string.IsNullOrWhiteSpace(value)) {
-                    if (_nazwa != value) {
-                        ZmianaNazwyEventArgs args = new ZmianaNazwyEventArgs();
-                        args.IstniejacaNazwa = _nazwa;
-                        args.NowaNazwa = value;
-
-                        ZmianaNazwy(this, args);
-                    }
-                    _nazwa = value;
+                if (string.IsNullOrWhiteSpace(value)) {
+                    throw new ArgumentNullException("Nazwa karty nie może być pusta");
                 }
+
+                if (_nazwa != value && ZmianaNazwy !=null) {
+                    ZmianaNazwyEventArgs args = new ZmianaNazwyEventArgs();
+                    args.IstniejacaNazwa = _nazwa;
+                    args.NowaNazwa = value;
+
+                    ZmianaNazwy(this, args);
+                }
+                _nazwa = value;
+
             }
         }
 
         //Event(zdarzeznie)
         public event ZmianaNazwyDelegatDelegate ZmianaNazwy;
 
-      
+
 
     }
 }
---
+++ Throw.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kartaOcenFilmow {
+    internal class Throw {
+        //static void Metoda() {
+        //    Console.WriteLine("Podaj liczbe");
+        //    int wiek = int.Parse(Console.ReadLine());
+
+        //    if (wiek == 20) {
+        //        throw new ArgumentException("20 jest niedozwolona wartoscia do wprowadzenia!");
+        //    }
+        //}
+
+        static void Metoda() {
+           
+        }
+    }
+}
